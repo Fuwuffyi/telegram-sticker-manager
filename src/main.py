be5a0625bf6cea2347981460c635e85d91e12bfd
fuwuffy <fuwuffyderg@proton.me>
2025-12-18 23:18:15 +0100

    Setup bot to read sticker messages

--- src/main.py
+++ src/main.py
@@ -1,14 +1,15 @@
 import os
 import json
-import shutil
-import asyncio
 import logging
-import aiohttp
-from pathlib import Path
+
 from dotenv import load_dotenv
 
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler, CallbackQueryHandler
+from pathlib import Path
+import asyncio
+import aiohttp
+
+from telegram import Update
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes
 
 # Configure logging
 logging.basicConfig(
@@ -17,12 +18,22 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+async def handle_sticker_pack(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    print(update)
+    print(context)
+
 def main() -> None:
     _ = load_dotenv()
     telegram_token: str | None = os.getenv("BOT_TOKEN")
     if not telegram_token:
         exit(-1)
     application = ApplicationBuilder().token(telegram_token).build()
+     # Register handlers
+    handlers = [
+        MessageHandler(filters.Sticker.ALL, handle_sticker_pack),
+    ]
+    for handler in handlers:
+        application.add_handler(handler)
     # Run the bot
     application.run_polling()
 
